2b40cf6e5c7f54602f3ad062b1f94fb0d6baa0cd
Shetty Yttehs <shetty.yttehs@outlook.com>
2026-05-29 22:39:00 +0800

    fix(hypridle): correct swaylock package

--- modules/home/desktop/hyprland/hypridle.nix
+++ modules/home/desktop/hyprland/hypridle.nix
@@ -1,23 +1,27 @@
 { pkgs, ... }:
+let
+  screenLockCmd = "swaylock";
+  displayOffCmd = "hyprctl dispatch dpms off";
+  displayResumeCmd = "hyprctl dispatch dpms on";
+in
 {
   services.hypridle = {
     enable = true;
-    package = pkgs.hypridle;
     settings = {
       general = {
-        after_sleep_cmd = "hyprctl dispatch dpms on";
+        after_sleep_cmd = displayResumeCmd;
         ignore_dbus_inhibit = false;
-        lock_cmd = "swaylock-themed";
+        lock_cmd = screenLockCmd;
       };
       listener = [
         {
           timeout = 300;
-          on-timeout = "swaylock-themed";
+          on-timeout = screenLockCmd;
         }
         {
           timeout = 600;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
+          on-timeout = displayOffCmd;
+          on-resume = displayResumeCmd;
         }
       ];
     };
