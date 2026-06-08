cb71e9631a7829f215ea19accbd19411982446e7
Shetty Yttehs <shetty.yttehs@outlook.com>
2026-06-08 18:26:47 +0800

    fix(swaylock): use the -f flag to prevent swaylock from forking and exiting immediately

--- modules/home/desktop/common/swayidle.nix
+++ modules/home/desktop/common/swayidle.nix
@@ -1,25 +1,45 @@
 { pkgs, ... }:
 
 let
-  screenLockCmd = "${pkgs.swaylock-effects}/bin/swaylock";
+  screenLockCmd = "${pkgs.swaylock-effects}/bin/swaylock -f";
+  monitorOffCmd = "${pkgs.niri}/bin/niri msg action power-off-monitors";
+  monitorOnCmd = "${pkgs.niri}/bin/niri msg action power-on-monitors";
 in
 {
   services.swayidle = {
     enable = true;
+
+    extraArgs = [ "-w" ];
+
     timeouts = [
       {
         timeout = 300;
         command = screenLockCmd;
       }
       {
-        timeout = 600;
-        command = "${pkgs.niri}/bin/niri msg action power-off-monitors";
-        resumeCommand = "${pkgs.niri}/bin/niri msg action power-on-monitors";
+        timeout = 330;
+        command = monitorOffCmd;
+        resumeCommand = monitorOnCmd;
       }
     ];
-    events = {
-      before-sleep = screenLockCmd;
-      lock = screenLockCmd;
-    };
+
+    events = [
+      {
+        event = "before-sleep";
+        command = screenLockCmd;
+      }
+      {
+        event = "lock";
+        command = screenLockCmd;
+      }
+      {
+        event = "unlock";
+        command = monitorOnCmd;
+      }
+      {
+        event = "after-resume";
+        command = monitorOnCmd;
+      }
+    ];
   };
 }
